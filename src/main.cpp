634190c2a0bf9273828c0bdb8ef939c33bc60f29
aloehr-1097 <andreas.loehr97@gmail.com>
2025-03-30 00:38:41 +0100

    lsp working, include dirs discovered

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include <string>
 #include "include/lib_module.h"
 #include "include/project-nameConfig.h"
+#include <fmt/core.h>
 
 int main(int argc, char *argv[]) {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "This is version "<< VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
 
+    fmt::print("Hello, {}!\n", "world");
     std::string s = "Hello from main!";
     cust_func(s);
     return 0;
